06c6ca55ace712988606fb77a2a74e1609c0185b
njedidi <33257897+njedidi@users.noreply.github.com>
2021-04-16 12:53:33 -0400

    Add files via upload

--- MDFN/run.sh
+++ MDFN/run.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/bash
 
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --mem=32G 
-#SBATCH --time=0 # No time limit
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1  # use 1 gpu
-
-module load anaconda3
-source activate conv_dia_2
-
 python run_MDFN.py \
 --data_dir datasets/mutual \
 --model_name_or_path \
